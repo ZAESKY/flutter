a13d31f5692a4a5c52efaa2d486f32b49cfc72fa
ZAESKY <986682545@qq.com>
2024-12-26 00:45:28 +0800

    增加PDF预览功能

--- lib/pages/detail.dart
+++ lib/pages/detail.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_cached_pdfview/flutter_cached_pdfview.dart';
+
 class detail extends StatefulWidget {
-  final String title;
-  const detail({super.key, required this.title});
+  const detail({super.key,});
 
   @override
   State<detail> createState() => _detailState();
@@ -15,7 +16,19 @@ class _detailState extends State<detail> {
         appBar: AppBar(
           title: Text('使用手册'),
         ),
-        body: Text('详情页' + widget.title),
+        body:  PDF(
+          enableSwipe: true,
+          swipeHorizontal: false,
+          autoSpacing: false,
+          pageFling: false,
+          backgroundColor: Colors.grey,
+          onError: (error) {
+            print(error.toString());
+          },
+          onPageError: (page, error) {
+            print('$page: ${error.toString()}');
+          },
+        ).fromAsset('assets/examples.pdf'),
       ),
     );
   }
--- lib/pages/list.dart
+++ lib/pages/list.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-import './detail.dart';
 import './pdf_preview.dart';
-
+import './detail.dart';
 class list extends StatefulWidget {
   final String items;
 
@@ -17,10 +16,14 @@ class _listState extends State<list> with AutomaticKeepAliveClientMixin{
     return GestureDetector(
       onTap: () {
         Navigator.push(context, MaterialPageRoute(builder: (BuildContext ctx) {
-          return new PDFViewerFromFile(filePath: '../pdf.pdf',);
+          return new detail();
         }));
       },
-      child: Text('这里是列表' + widget.items),
+      child: Card(
+        child: ListTile(
+          title: Text('使用手册PDF'),
+        ),
+      ),
     );
   }
 
